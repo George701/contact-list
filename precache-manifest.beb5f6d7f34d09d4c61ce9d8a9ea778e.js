3d6917d148fc4c83c78addc159e3e4c17b88bd08
George701 <chernikov.georgii@gmail.com>
2019-01-26 16:34:45 +0100

    Updates

---
+++ precache-manifest.beb5f6d7f34d09d4c61ce9d8a9ea778e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "377b31ea3711a9d6ba50",
+    "url": "/contact-list/static/js/runtime~main.377b31ea.js"
+  },
+  {
+    "revision": "534d594926ad3a41d731",
+    "url": "/contact-list/static/js/main.534d5949.chunk.js"
+  },
+  {
+    "revision": "f6790c268a053250d986",
+    "url": "/contact-list/static/js/1.f6790c26.chunk.js"
+  },
+  {
+    "revision": "534d594926ad3a41d731",
+    "url": "/contact-list/static/css/main.43048e55.chunk.css"
+  },
+  {
+    "revision": "f6790c268a053250d986",
+    "url": "/contact-list/static/css/1.a99b80ba.chunk.css"
+  },
+  {
+    "revision": "3032d815ab351ed05f60187839e778dc",
+    "url": "/contact-list/index.html"
+  }
+];
